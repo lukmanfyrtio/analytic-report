b2e844b70c8818a62f01f8fa75a6a97de2fc6c29
lukmanfyrtio <lukman.fyrtio@gmail.com>
2023-10-17 09:26:39 +0700

    push latest update

--- src/main/java/org/wso2/am/analytics/publisher/sample/reporter/CustomDataUsageDAO.java
+++ src/main/java/org/wso2/am/analytics/publisher/sample/reporter/CustomDataUsageDAO.java
@@ -9,148 +9,119 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.wso2.carbon.apimgt.impl.utils.APIMgtDBUtil;
 
 public class CustomDataUsageDAO {
-
-	private static final Logger log = LoggerFactory.getLogger(CustomDataUsageDAO.class);
-
-	private static CustomDataUsageDAO INSTANCE = null;
-
-//	private ApiMgtDAO apiMgtDAO = ApiMgtDAO.getInstance();
-
-	public static CustomDataUsageDAO getInstance() {
-		if (INSTANCE == null) {
-			INSTANCE = new CustomDataUsageDAO();
-		}
-		return INSTANCE;
-	}
-
-	public void addApiUsage(MetricData metricData) throws Exception {
-		Connection conn = null;
-
-		try {
-			conn = APIMgtDBUtil.getConnection();
-			conn.setAutoCommit(false);
-
-			String insertQuery = "INSERT INTO DATA_USAGE_API (API_NAME, PROXY_RESPONSE_CODE, DESTINATION, API_CREATOR_TENANT_DOMAIN, PLATFORM, API_METHOD, API_VERSION, GATEWAY_TYPE, API_CREATOR, RESPONSE_CACHE_HIT, BACKEND_LATENCY, CORRELATION_ID, REQUEST_MEDIATION_LATENCY, KEY_TYPE, API_ID, APPLICATION_NAME, TARGET_RESPONSE_CODE, REQUEST_TIMESTAMP, APPLICATION_OWNER, USER_AGENT, EVENT_TYPE, API_RESOURCE_TEMPLATE, RESPONSE_LATENCY, REGION_ID, RESPONSE_MEDIATION_LATENCY, USER_IP, APPLICATION_ID, API_TYPE,SUBSCRIPTION_ID) "
-					+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-			PreparedStatement insertStmt = conn.prepareStatement(insertQuery);
-			insertStmt.setString(1, metricData.getApiName());
-			insertStmt.setInt(2, metricData.getProxyResponseCode());
-			insertStmt.setString(3, metricData.getDestination());
-			insertStmt.setString(4, metricData.getApiCreatorTenantDomain());
-			insertStmt.setString(5, metricData.getPlatform());
-			insertStmt.setString(6, metricData.getApiMethod());
-			insertStmt.setString(7, metricData.getApiVersion());
-			insertStmt.setString(8, metricData.getGatewayType());
-			insertStmt.setString(9, metricData.getApiCreator());
-			insertStmt.setBoolean(10, metricData.isResponseCacheHit());
-			insertStmt.setInt(11, metricData.getBackendLatency());
-			insertStmt.setString(12, metricData.getCorrelationId());
-			insertStmt.setInt(13, metricData.getRequestMediationLatency());
-			insertStmt.setString(14, metricData.getKeyType());
-			insertStmt.setString(15, metricData.getApiId());
-			insertStmt.setString(16, metricData.getApplicationName());
-			insertStmt.setInt(17, metricData.getTargetResponseCode());
-	        // Convert the string to an Instant
-	        Instant instant = Instant.parse(metricData.getRequestTimestamp());
-
-	        // Convert the Instant to a LocalDateTime
-	        LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
-
-	        // Convert the LocalDateTime to a Date
-	        Date date = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
-			insertStmt.setTimestamp(18, new Timestamp(date.getTime()));
-			insertStmt.setString(19, metricData.getApplicationOwner());
-			insertStmt.setString(20, metricData.getUserAgent());
-			insertStmt.setString(21, metricData.getEventType());
-			insertStmt.setString(22, metricData.getApiResourceTemplate());
-			insertStmt.setInt(23, metricData.getResponseLatency());
-			insertStmt.setString(24, metricData.getRegionId());
-			insertStmt.setInt(25, metricData.getResponseMediationLatency());
-			insertStmt.setString(26, metricData.getUserIp());
-			insertStmt.setString(27, metricData.getApplicationId());
-			insertStmt.setString(28, metricData.getApiType());
-			PlanData planData=getPlanDataBySubscriberId(metricData.getApiId(), metricData.getApplicationId());
-			insertStmt.setString(29, planData.getSubscriptionId());
-			insertStmt.executeUpdate();
-			conn.commit();
-			log.info("API Data Usage inserted successfully.");
-//			}
-
-		} catch (SQLException e) {
-			if (conn != null) {
-				try {
-					conn.rollback();
-				} catch (SQLException ex) {
-					String errorMessage = "Failed to rollback adding data usage for : "
-							+ metricData.getApplicationName();
-					log.error(errorMessage);
-					throw new CustomDataUsageException(errorMessage, ex);
-				}
-			}
-			String errorMessage = "Failed to add data usage for : " + metricData.getApplicationName();
-			log.error(errorMessage);
-			throw new CustomDataUsageException(errorMessage, e);
-		}
-
-	}
-	
-	public PlanData getPlanDataBySubscriberId(String string,String string2) throws Exception {
-
-		Connection conn = null;
-		PreparedStatement ps = null;
-		ResultSet result = null;
-		PlanData monetizedSubscription = new PlanData();
-		String sqlQuery = GET_BE_PLAN_BY_SUBSCRIBED_ID_SQL;
-		try {
-			conn = APIMgtDBUtil.getConnection();
-			ps = conn.prepareStatement(sqlQuery);
-			ps.setString(1, string);
-			ps.setString(2, string2);
-			result = ps.executeQuery();
-			log.info(ps.toString());
-			log.info("Parameter API ID : "+string);
-			log.info("Parameter APP ID : "+string2);
-			if (result.next()) {
-				monetizedSubscription.setSubscriptionId(result.getString("SUBSCRIPTION_ID"));
-				monetizedSubscription.setMonetizationPlan(result.getString("MONETIZATION_PLAN"));
-				monetizedSubscription.setBillingCycle(result.getString("BILLING_CYCLE"));
-				monetizedSubscription.setCurrency(result.getString("CURRENCY"));
-				monetizedSubscription.setFixedRate(result.getString("FIXED_RATE"));
-				monetizedSubscription.setPricePerRequest(result.getString("PRICE_PER_REQUEST"));
-			}
-		} catch (SQLException e) {
-			String errorMessage = "Failed to get billing engine Subscription info for API ID : " +string ;
-			log.error(errorMessage);
-			throw new Exception(errorMessage, e);
-		} finally {
-			APIMgtDBUtil.closeAllConnections(ps, conn, result);
-		}
-		return monetizedSubscription;
-	}
-	
-    public static final String GET_BE_PLAN_BY_SUBSCRIBED_ID_SQL = "SELECT " +
-    		        "ASN.SUBSCRIPTION_ID, " +
-    		        "APS.MONETIZATION_PLAN, " +
-    		        "APS.BILLING_CYCLE, " +
-    		        "APS.FIXED_RATE, " +
-    		        "APS.PRICE_PER_REQUEST, " +
-    		        "APS.CURRENCY " +
-    		        "FROM " +
-    		        "AM_POLICY_SUBSCRIPTION APS " +
-    		        "JOIN " +
-    		        "AM_SUBSCRIPTION ASN ON ASN.TIER_ID = APS.NAME " +
-    		        "JOIN " +
-    		        "AM_API AA ON AA.API_ID = ASN.API_ID " +
-    		        "JOIN " +
-    		        "AM_APPLICATION APP ON APP.APPLICATION_ID = ASN.APPLICATION_ID " +
-    		        "WHERE " +
-    		        "AA.API_UUID = ? " +
-    		        "AND APP.UUID = ?";
+  private static final Logger log = LoggerFactory.getLogger(CustomDataUsageDAO.class);
+  
+  private static CustomDataUsageDAO INSTANCE = null;
+  
+  public static final String GET_BE_PLAN_BY_SUBSCRIBED_ID_SQL = "SELECT ASN.SUBSCRIPTION_ID, ASN.UUID, APS.MONETIZATION_PLAN, APS.BILLING_CYCLE, APS.FIXED_RATE, APS.PRICE_PER_REQUEST, APS.CURRENCY, APS.BILLING_PLAN FROM AM_POLICY_SUBSCRIPTION APS JOIN AM_SUBSCRIPTION ASN ON ASN.TIER_ID = APS.NAME JOIN AM_API AA ON AA.API_ID = ASN.API_ID JOIN AM_APPLICATION APP ON APP.APPLICATION_ID = ASN.APPLICATION_ID WHERE AA.API_UUID = ? AND APP.UUID = ?";
+  
+  public static CustomDataUsageDAO getInstance() {
+    if (INSTANCE == null)
+      INSTANCE = new CustomDataUsageDAO(); 
+    return INSTANCE;
+  }
+  
+  public void addApiUsage(MetricData metricData) throws Exception {
+    Connection conn = null;
+    conn = APIMgtDBUtil.getConnection();
+    conn.setAutoCommit(false);
+    try {
+      PlanData planData = getPlanDataBySubscriberId(metricData.getApiId(), metricData.getApplicationId());
+      if (!"FREE".equalsIgnoreCase(planData.getBillingPlan())) {
+        String insertQuery = "INSERT INTO DATA_USAGE_API (API_NAME, PROXY_RESPONSE_CODE, DESTINATION, API_CREATOR_TENANT_DOMAIN, PLATFORM, API_METHOD, API_VERSION, GATEWAY_TYPE, API_CREATOR, RESPONSE_CACHE_HIT, BACKEND_LATENCY, CORRELATION_ID, REQUEST_MEDIATION_LATENCY, KEY_TYPE, API_ID, APPLICATION_NAME, TARGET_RESPONSE_CODE, REQUEST_TIMESTAMP, APPLICATION_OWNER, USER_AGENT, EVENT_TYPE, API_RESOURCE_TEMPLATE, RESPONSE_LATENCY, REGION_ID, RESPONSE_MEDIATION_LATENCY, USER_IP, APPLICATION_ID, API_TYPE,SUBSCRIPTION_ID,SUBSCRIPTION_UUID) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        PreparedStatement insertStmt = conn.prepareStatement(insertQuery);
+        insertStmt.setString(1, metricData.getApiName());
+        insertStmt.setInt(2, metricData.getProxyResponseCode());
+        insertStmt.setString(3, metricData.getDestination());
+        insertStmt.setString(4, metricData.getApiCreatorTenantDomain());
+        insertStmt.setString(5, metricData.getPlatform());
+        insertStmt.setString(6, metricData.getApiMethod());
+        insertStmt.setString(7, metricData.getApiVersion());
+        insertStmt.setString(8, metricData.getGatewayType());
+        insertStmt.setString(9, metricData.getApiCreator());
+        insertStmt.setBoolean(10, metricData.isResponseCacheHit());
+        insertStmt.setInt(11, metricData.getBackendLatency());
+        insertStmt.setString(12, metricData.getCorrelationId());
+        insertStmt.setInt(13, metricData.getRequestMediationLatency());
+        insertStmt.setString(14, metricData.getKeyType());
+        insertStmt.setString(15, metricData.getApiId());
+        insertStmt.setString(16, metricData.getApplicationName());
+        insertStmt.setInt(17, metricData.getTargetResponseCode());
+        Instant instant = Instant.parse(metricData.getRequestTimestamp());
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+        Date date = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+        insertStmt.setTimestamp(18, new Timestamp(date.getTime()));
+        insertStmt.setString(19, metricData.getApplicationOwner());
+        insertStmt.setString(20, metricData.getUserAgent());
+        insertStmt.setString(21, metricData.getEventType());
+        insertStmt.setString(22, metricData.getApiResourceTemplate());
+        insertStmt.setInt(23, metricData.getResponseLatency());
+        insertStmt.setString(24, metricData.getRegionId());
+        insertStmt.setInt(25, metricData.getResponseMediationLatency());
+        insertStmt.setString(26, metricData.getUserIp());
+        insertStmt.setString(27, metricData.getApplicationId());
+        insertStmt.setString(28, metricData.getApiType());
+        insertStmt.setString(29, planData.getSubscriptionId());
+        insertStmt.setString(30, planData.getSubscriptionIdUuid());
+        insertStmt.executeUpdate();
+        conn.commit();
+        insertStmt.close();
+        log.debug("API Data Usage inserted successfully.");
+      } 
+    } catch (SQLException e) {
+      if (conn != null)
+        try {
+          conn.rollback();
+        } catch (SQLException ex) {
+          String str = "Failed to rollback adding data usage for : " + metricData.getApplicationName();
+          log.error(str);
+          throw new CustomDataUsageException(str, ex);
+        }  
+      String errorMessage = "Failed to add data usage for : " + metricData.getApplicationName();
+      log.error(errorMessage);
+      throw new CustomDataUsageException(errorMessage, e);
+    } finally {
+      conn.close();
+    } 
+  }
+  
+  public PlanData getPlanDataBySubscriberId(String string, String string2) throws Exception {
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet result = null;
+    PlanData monetizedSubscription = new PlanData();
+    String sqlQuery = "SELECT ASN.SUBSCRIPTION_ID, ASN.UUID, APS.MONETIZATION_PLAN, APS.BILLING_CYCLE, APS.FIXED_RATE, APS.PRICE_PER_REQUEST, APS.CURRENCY, APS.BILLING_PLAN FROM AM_POLICY_SUBSCRIPTION APS JOIN AM_SUBSCRIPTION ASN ON ASN.TIER_ID = APS.NAME JOIN AM_API AA ON AA.API_ID = ASN.API_ID JOIN AM_APPLICATION APP ON APP.APPLICATION_ID = ASN.APPLICATION_ID WHERE AA.API_UUID = ? AND APP.UUID = ?";
+    try {
+      conn = APIMgtDBUtil.getConnection();
+      ps = conn.prepareStatement(sqlQuery);
+      ps.setString(1, string);
+      ps.setString(2, string2);
+      result = ps.executeQuery();
+      log.debug(ps.toString());
+      log.debug("Parameter API ID : " + string);
+      log.debug("Parameter APP ID : " + string2);
+      if (result.next()) {
+        monetizedSubscription.setSubscriptionId(result.getString("SUBSCRIPTION_ID"));
+        monetizedSubscription.setMonetizationPlan(result.getString("MONETIZATION_PLAN"));
+        monetizedSubscription.setBillingCycle(result.getString("BILLING_CYCLE"));
+        monetizedSubscription.setCurrency(result.getString("CURRENCY"));
+        monetizedSubscription.setFixedRate(result.getString("FIXED_RATE"));
+        monetizedSubscription.setPricePerRequest(result.getString("PRICE_PER_REQUEST"));
+        monetizedSubscription.setSubscriptionIdUuid(result.getString("UUID"));
+        monetizedSubscription.setBillingPlan(result.getString("BILLING_PLAN"));
+      } 
+    } catch (SQLException e) {
+      String errorMessage = "Failed to get billing engine Subscription info for API ID : " + string;
+      log.error(errorMessage);
+      throw new Exception(errorMessage, e);
+    } finally {
+      APIMgtDBUtil.closeAllConnections(ps, conn, result);
+    } 
+    return monetizedSubscription;
+  }
 }
--- src/main/java/org/wso2/am/analytics/publisher/sample/reporter/LogCounterMetric.java
+++ src/main/java/org/wso2/am/analytics/publisher/sample/reporter/LogCounterMetric.java
@@ -1,7 +1,8 @@
 package org.wso2.am.analytics.publisher.sample.reporter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.wso2.am.analytics.publisher.exception.MetricReportingException;
@@ -11,68 +12,96 @@ import org.wso2.am.analytics.publisher.reporter.MetricSchema;
 import org.wso2.am.analytics.publisher.reporter.cloud.DefaultFaultMetricEventBuilder;
 import org.wso2.am.analytics.publisher.reporter.cloud.DefaultResponseMetricEventBuilder;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
-
 public class LogCounterMetric implements CounterMetric {
-
-	private static final Logger log = LoggerFactory.getLogger(LogCounterMetric.class);
-	private String name;
-	private MetricSchema schema;
-	private final Gson gson;
-	private CustomDataUsageDAO customDataUsageDAO=CustomDataUsageDAO.getInstance();
-
-	public LogCounterMetric(String name, MetricSchema schema) {
-		this.name = name;
-		this.schema = schema;
-		this.gson = new Gson();
-	}
-
-	@Override
-	public int incrementCount(MetricEventBuilder metricEventBuilder) throws MetricReportingException {
-		Map<String, Object> event = metricEventBuilder.build();
-		String jsonString = gson.toJson(event);
-		log.info("apimMetrics: " + name.replaceAll("[\r\n]", "") + ", properties :"
-				+ jsonString.replaceAll("[\r\n]", ""));
-
-		ObjectMapper objectMapper = new ObjectMapper();
-
-		try {
-			MetricData apiResponse = objectMapper.readValue(jsonString, MetricData.class);
-			log.info("application name ------- "+apiResponse.getApplicationName());
-			log.info("application id ------- "+apiResponse.getApplicationId());
-			log.info("application owner ------- "+apiResponse.getApplicationOwner());
-			log.info("destination ------- "+apiResponse.getDestination());
-			log.info("API method ------- "+apiResponse.getApiMethod());
-			log.info("API version ------- "+apiResponse.getApiVersion());
-			log.info("user IP ------- "+apiResponse.getUserIp());
-			customDataUsageDAO.addApiUsage(apiResponse);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return 0;
-	}
-
-	@Override
-	public String getName() {
-		return this.name;
-	}
-
-	@Override
-	public MetricSchema getSchema() {
-		return this.schema;
-	}
-
-	@Override
-	public MetricEventBuilder getEventBuilder() {
-		switch (schema) {
-		case RESPONSE:
-			return new DefaultResponseMetricEventBuilder();
-		case ERROR:
-			return new DefaultFaultMetricEventBuilder();
-		default:
-			// will not happen
-			return null;
-		}
-	}
+  private static final Logger log = LoggerFactory.getLogger(LogCounterMetric.class);
+  
+  private String name;
+  
+  private MetricSchema schema;
+  
+  private final Gson gson;
+  
+  private CustomDataUsageDAO customDataUsageDAO = CustomDataUsageDAO.getInstance();
+  
+  public LogCounterMetric(String name, MetricSchema schema) {
+    this.name = name;
+    this.schema = schema;
+    this.gson = new Gson();
+  }
+  
+  public int incrementCount(MetricEventBuilder metricEventBuilder) throws MetricReportingException {
+    Map<String, Object> event = metricEventBuilder.build();
+    String jsonString = this.gson.toJson(event);
+    log.info("apimMetrics: " + this.name.replaceAll("[\r\n]", "") + ", properties :" + jsonString
+        .replaceAll("[\r\n]", ""));
+    ObjectMapper objectMapper = new ObjectMapper();
+    try {
+      MetricData metricData = new MetricData();
+      metricData.setApiName((String)event.getOrDefault("apiName", ""));
+      metricData
+        .setProxyResponseCode(Integer.valueOf(String.valueOf(event.getOrDefault("proxyResponseCode", Integer.valueOf(0)))).intValue());
+      metricData.setDestination((String)event.getOrDefault("destination", ""));
+      metricData.setApiCreatorTenantDomain((String)event.getOrDefault("apiCreatorTenantDomain", ""));
+      metricData.setPlatform((String)event.getOrDefault("platform", ""));
+      metricData.setApiMethod((String)event.getOrDefault("apiMethod", ""));
+      metricData.setApiVersion((String)event.getOrDefault("apiVersion", ""));
+      metricData.setGatewayType((String)event.getOrDefault("gatewayType", ""));
+      metricData.setApiCreator((String)event.getOrDefault("apiCreator", ""));
+      metricData.setResponseCacheHit(((Boolean)event.getOrDefault("responseCacheHit", Boolean.valueOf(false))).booleanValue());
+      metricData.setBackendLatency(Integer.valueOf(String.valueOf(event.getOrDefault("backendLatency", Integer.valueOf(0)))).intValue());
+      metricData.setCorrelationId((String)event.getOrDefault("correlationId", ""));
+      metricData.setRequestMediationLatency(
+          Integer.valueOf(String.valueOf(event.getOrDefault("requestMediationLatency", Integer.valueOf(0)))).intValue());
+      metricData.setKeyType((String)event.getOrDefault("keyType", ""));
+      metricData.setApiId((String)event.getOrDefault("apiId", ""));
+      metricData.setApplicationName((String)event.getOrDefault("applicationName", ""));
+      metricData.setTargetResponseCode(
+          Integer.valueOf(String.valueOf(event.getOrDefault("targetResponseCode", Integer.valueOf(0)))).intValue());
+      metricData.setRequestTimestamp((String)event.getOrDefault("requestTimestamp", ""));
+      metricData.setApplicationOwner((String)event.getOrDefault("applicationOwner", ""));
+      metricData.setUserAgent((String)event.getOrDefault("userAgent", ""));
+      metricData.setEventType((String)event.getOrDefault("eventType", ""));
+      metricData.setApiResourceTemplate((String)event.getOrDefault("apiResourceTemplate", ""));
+      metricData.setResponseLatency(Integer.valueOf(String.valueOf(event.getOrDefault("responseLatency", Integer.valueOf(0)))).intValue());
+      metricData.setRegionId((String)event.getOrDefault("regionId", ""));
+      metricData.setResponseMediationLatency(
+          Integer.valueOf(String.valueOf(event.getOrDefault("responseMediationLatency", Integer.valueOf(0)))).intValue());
+      metricData.setUserIp((String)event.getOrDefault("userIp", ""));
+      metricData.setApplicationId((String)event.getOrDefault("applicationId", ""));
+      metricData.setApiType((String)event.getOrDefault("apiType", ""));
+      metricData.setErrorType((String)event.getOrDefault("errorType", ""));
+      metricData.setErrorMessage((String)event.getOrDefault("errorMessage", ""));
+      metricData.setErrorCode((String)event.getOrDefault("errorCode", ""));
+      log.debug("application name ------- " + metricData.getApplicationName());
+      log.debug("application id ------- " + metricData.getApplicationId());
+      log.debug("application owner ------- " + metricData.getApplicationOwner());
+      log.debug("destination ------- " + metricData.getDestination());
+      log.debug("API method ------- " + metricData.getApiMethod());
+      log.debug("API version ------- " + metricData.getApiVersion());
+      log.debug("user IP ------- " + metricData.getUserIp());
+      this.customDataUsageDAO.addApiUsage(metricData);
+    } catch (Exception e) {
+      log.debug("Error when adding api usage to db : " + e.getMessage());
+      e.printStackTrace();
+    } 
+    return 0;
+  }
+  
+  public String getName() {
+    return this.name;
+  }
+  
+  public MetricSchema getSchema() {
+    return this.schema;
+  }
+  
+  public MetricEventBuilder getEventBuilder() {
+    switch (this.schema) {
+      case RESPONSE:
+        return (MetricEventBuilder)new DefaultResponseMetricEventBuilder();
+      case ERROR:
+        return (MetricEventBuilder)new DefaultFaultMetricEventBuilder();
+    } 
+    return null;
+  }
 }
